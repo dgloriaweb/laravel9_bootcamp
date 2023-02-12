bd49687600a5b94447067abd07e220c44b03c5dd
unknown <dgloria.web@gmail.com>
2023-02-12 06:14:24 +0000

    starting pages

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 run laravel server: php artisan serve
 run frontend: npm run dev 
-running url: http://localhost:8000/chirps
+running url: http://localhost:8000/
 
 on startup run php artisan migrate to get the tables
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
